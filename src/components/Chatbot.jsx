4c28ee5f7ba245f56bf280658ba1c4d3d825646b
Jose-01010000 <josepuello112000@gmail.com>
2023-04-22 08:28:39 -0500

    Optimizado componente chatbot y ventana modal.

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -1,28 +1,110 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function ChatBot() {
-  const [messages, setMessages] = useState([]);
+  // const [messages, setMessages] = useState([]);
+  const [inputValue, setInputValue] = useState("");
 
-  const addMessage = (message) => {
-    setMessages([...messages, message]);
-  };
+  const [conversation, setConversation] = useState([]);
+  const [message, setMessage] = useState("");
+  const [feedback, setFeedback] = useState(null);
 
-  const handleKeyPress = (event) => {
-    if (event.key === "Enter") {
-      addMessage(event.target.value);
-      event.target.value = "";
+  // const addMessage = (message) => {
+  //   setMessages([...messages, message]);
+  // };
+
+  // const handleKeyPress = (event) => {
+  //   if (event.key === "Enter") {
+  //     addMessage(event.target.value);
+  //     event.target.value = "";
+  //   }
+  // };
+
+  // const handleOnChange = (event) => {
+  //   addMessage(event.target.value);
+  //   event.target.value = "";
+  // };
+
+  const handleSendMessage = async () => {
+    try {
+      const response = await axios.post("http://localhost:5000/train", {
+        userQuery: message,
+        intent: "welcome",
+        feedback: feedback,
+      });
+
+      setConversation([
+        ...conversation,
+        {
+          message,
+          sender: "user",
+        },
+        {
+          message: response.data.message,
+          sender: "bot",
+        },
+      ]);
+      setMessage("");
+      setFeedback(null);
+    } catch (error) {
+      console.error(error);
     }
   };
 
-  const handleOnChange = (event) => {
-    addMessage(event.target.value);
-    event.target.value = "";
+  const handleFeedback = (feedback) => {
+    setFeedback(feedback);
   };
+
+  // useEffect(() => {
+  //   sendMessage("Hola");
+  // }, []);
+
+  // const sendMessage = async (message) => {
+  //   setMessages((prevState) => [
+  //     ...prevState,
+  //     { text: message, sender: "user" },
+  //   ]);
+  //   setInputValue("");
+
+  //   const response = await axios.post(
+  //     `https://dialogflow.googleapis.com/v2/projects/TU_PROYECTO_AGENT_ID/sessions/TU_SESION_DE_CHATBOT:detectIntent`,
+  //     {
+  //       queryInput: {
+  //         text: {
+  //           text: message,
+  //           languageCode: "es-ES",
+  //         },
+  //       },
+  //     },
+  //     {
+  //       headers: {
+  //         Authorization: `Bearer ${process.env.REACT_APP_DIALOGFLOW_API_KEY}`,
+  //         "Content-Type": "application/json; charset=utf-8",
+  //       },
+  //     }
+  //   );
+
+  //   const responseMessage = response.data.queryResult.fulfillmentText;
+  //   setMessages((prevState) => [
+  //     ...prevState,
+  //     { text: responseMessage, sender: "bot" },
+  //   ]);
+  // };
+
+  // const handleInputValueChange = (event) => {
+  //   setInputValue(event.target.value);
+  // };
+
+  // const handleFormSubmit = (event) => {
+  //   event.preventDefault();
+  //   sendMessage(inputValue);
+  // };
 
   return (
     <div className="flex flex-col h-full">
-      {/* <div className="flex-1 overflow-y-auto overflow-auto p-4">
-        {messages.map((message, index) => (
+      <h1 class="mt-1 text-lg font-semibold text-black ">InterviewBuddy</h1>
+      <div className="flex-1 overflow-y-auto overflow-auto p-4">
+        {conversation.map((message, index) => (
           <div
             key={index}
             className={`my-2 ${index % 2 === 0 ? "text-right" : "text-left"}`}
@@ -36,23 +118,47 @@ function ChatBot() {
             </div>
           </div>
         ))}
-      </div> */}
-      <div className="p-4" >
-          
       </div>
+      <div className="p-4"></div>
       <div className="flex items-center p-4">
         <input
           type="text"
           placeholder="Escribe un mensaje..."
           className="flex-1 border-gray-300 border-2 rounded-full p-2 mr-2"
-          onChange={handleKeyPress}
-        /> 
+          onChange={(event) => setMessage(event.target.value)}
+          value={message}
+        />
         <button
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
-          onClick={handleKeyPress()}
+          onClick={handleSendMessage}
         >
           Enviar
         </button>
+      </div>
+      <h1 class="mt-4 text-lg font-semibold text-black ">Retroalimentación</h1>
+      <div className="flex items-start p-4">
+        {feedback === null && (
+          <div>
+            <button
+              className="bg-green-500 hover:bg-green-700 font-bold text-white py-2 ml-3 px-4 rounded-full"
+              onClick={() => handleFeedback("bien")}
+            >
+              Bien
+            </button>
+            <button
+              className="bg-yellow-500 hover:bg-yellow-700 font-bold text-white py-2 ml-3 px-4 rounded-full"
+              onClick={() => handleFeedback("regular")}
+            >
+              Regular
+            </button>
+            <button
+              className="bg-red-500 hover:bg-red-700 font-bold text-white py-2 ml-3 px-4 rounded-full "
+              onClick={() => handleFeedback("mal")}
+            >
+              Mal
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -1,25 +1,25 @@
 import React from "react";
 
 function Modal({ isOpen, onClose, children }) {
-    console.log(isOpen)
+  console.log(isOpen);
   if (!isOpen) return null;
 
   return (
     <div className="fixed z-10 inset-0 overflow-y-auto">
-      <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+      <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center lg:block lg:p-0">
         <div className="fixed inset-0 transition-opacity">
           <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
         </div>
-        <span className="hidden sm:inline-block sm:align-middle sm:h-screen"></span>
+        <span className="hidden lg:inline-block lg:align-middle lg:h-screen"></span>
         &#8203;
-        <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-          <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+        <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all lg:my-8 lg:align-middle lg:max-w-lg lg:w-full">
+          <div className="bg-white px-4 pt-5 pb-4 lg:p-6 lg:pb-4">
             {children}
           </div>
-          <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+          <div className="bg-gray-50 px-4 py-3 lg:px-6 lg:flex lg:flex-row-reverse">
             <button
               type="button"
-              className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-500 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"
+              className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-blue-500 text-base font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 lg:ml-3 lg:w-auto lg:text-sm"
               onClick={onClose}
             >
               Cerrar
