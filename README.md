21bc0018b4c18b6cda3f94395cf81c71b37dd0a7
Jose-01010000 <josepuello112000@gmail.com>
2023-05-25 02:22:45 -0500

    Updates

--- README.md
+++
@@ -1,4 +0,0 @@
-# InterviewBuddy  - ChatBot whit Modern UI/UX using React.js & Tailwind CSS
-
-![HooBank](https://github.com/Jose-01010000/chatbot_react_2.0/blob/main/src/assets/img.png)
-
--- src/components/index.js
+++
@@ -1,8 +0,0 @@
-import Navbar from "./Navbar";
-import Business from "./Business";
-import Stats from "./Stats";
-import Footer from "./Footer";
-import Hero from "./Hero";
-import CTA from "./CTA";
-
-export { Navbar, CTA, Business, Stats, Footer, Hero };
